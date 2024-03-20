a515adda3d5460b29f010e99e719c013d6939e7c
Slaiby <mallahslaiby@gmail.com>
2024-03-20 12:17:13 +0100

    Add PredictionRequest model to models.py

--- logic_layer/models.py
+++ logic_layer/models.py
@@ -1,8 +1,15 @@
 from pydantic import BaseModel
 
-
 class Prediction(BaseModel):
     id: int
     inference_id: str
     result: dict 
     timestamp: str
+    
+class PredictionRequest(BaseModel):
+    credit_history: float
+    dependents: str
+    education: str
+    married: str
+    property_area: int
+    coapplicant_income: float
