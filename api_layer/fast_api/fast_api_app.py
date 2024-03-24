46ecf9efc11c49e62d8cbf7c3b551bebbfe0988f
Slaiby <mallahslaiby@gmail.com>
2024-03-24 20:53:28 +0100

    Add insert_inference_data function to predict endpoint and update get_past_predictions endpoint

--- api_layer/fast_api/fast_api_app.py
+++ api_layer/fast_api/fast_api_app.py
@@ -3,7 +3,7 @@ from typing import List
 import json
 import numpy as np
 
-from api_layer.fast_api.service_slices import fetch_past_predictions
+from api_layer.fast_api.service_slices import fetch_past_predictions, insert_inference_data
 from logic_layer.pydantic_models import Prediction, PredictionRequest
 from logic_layer.acceptance_prediction.inference import make_predictions
 
@@ -13,15 +13,18 @@ app = FastAPI()
 async def predict(request_data: PredictionRequest):
     input_dict = request_data.model_dump()
     prediction = make_predictions(input_dict)
+    await insert_inference_data(input_dict, prediction.tolist()[0])
     return {"prediction": prediction.tolist()}
 
 @app.get("/get-past-predictions", response_model=List[Prediction])
 async def get_past_predictions():
-    predictions = await fetch_past_predictions()
-    prediction_list = [Prediction(
-        id=row['id'],
-        inference_id=row['inference_id'],
-        result=json.loads(row['result']) if isinstance(row['result'], str) else row['result'],
-        timestamp=row['timestamp'].isoformat()
-    ) for row in predictions]
+    predictions = await fetch_past_predictions() 
+    prediction_list = [
+        Prediction(
+            id=prediction['id'],
+            result=json.loads(prediction['request_data']),
+            prediction=str(prediction['prediction']),
+            timestamp=str(prediction['timestamp'])
+        ) for prediction in predictions     
+    ]
     return prediction_list
