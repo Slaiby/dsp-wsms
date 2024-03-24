cff69a8b8165458fe3c92ed0dfdd76162cd5dc03
Slaiby <mallahslaiby@gmail.com>
2024-03-24 20:53:32 +0100

    Add fetch_past_predictions and insert_inference_data functions

--- api_layer/fast_api/service_slices.py
+++ api_layer/fast_api/service_slices.py
@@ -1,3 +1,4 @@
+import json
 from logic_layer.postgres_database.database_utils import create_db_connection
 
 async def fetch_past_predictions():
@@ -5,3 +6,11 @@ async def fetch_past_predictions():
     rows = await conn.fetch('SELECT * FROM api_inferences')
     await conn.close()
     return rows
+
+async def insert_inference_data(data_input, prediction):
+    conn = await create_db_connection()
+    async with conn.transaction():
+        await conn.execute('''
+            INSERT INTO api_inferences (request_data, prediction) VALUES ($1, $2)
+        ''', json.dumps(data_input), str(prediction))
+    await conn.close()
