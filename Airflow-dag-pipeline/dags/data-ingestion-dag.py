9844b2eff687bb1cbca207aa87f0bc04c9daa3c9
WSatti <iwaleed.dev@gmail.com>
2024-03-26 14:41:44 +0500

    Added updated files for complete data ingestion in airflow

--- Airflow-dag-pipeline/dags/data-ingestion-dag.py
+++ Airflow-dag-pipeline/dags/data-ingestion-dag.py
@@ -7,13 +7,8 @@ import random
 import shutil
 
 def read_data():
-    # Get the directory where your DAG file resides
     dag_folder = os.path.dirname(os.path.abspath(__file__))
-    
-    # Construct the path to the raw-data folder
     raw_data_folder = os.path.join(dag_folder, 'raw-data')
-    
-    # Check if the folder exists and list files
     if os.path.exists(raw_data_folder):
         files = os.listdir(raw_data_folder)
         if files:
@@ -23,39 +18,54 @@ def read_data():
             return file_path
         else:
             print("No files available in raw-data folder.")
+            return None
     else:
         print("The raw-data folder does not exist.")
-
+        return None
 def save_file(**kwargs):
     file_path = kwargs['ti'].xcom_pull(task_ids='read_data')
     print("Received file path:", file_path)
     if file_path:
-        good_data_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "good-data") 
-        shutil.move(file_path, good_data_folder)
-        print(f"File moved to good-data folder: {file_path}")
+        if os.path.exists(file_path): 
+            good_data_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "good-data") 
+            file_name = os.path.basename(file_path)
+            destination_path = os.path.join(good_data_folder, file_name)
+            if os.path.exists(destination_path):
+                base_name, ext = os.path.splitext(file_name)
+                timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+                new_file_name = f"{base_name}_{timestamp}{ext}"
+                destination_path = os.path.join(good_data_folder, new_file_name)
+                print(f"File '{file_name}' already exists in 'good-data' folder. Renaming to '{new_file_name}'.")
+            shutil.move(file_path, destination_path)
+            print(f"File '{file_name}' moved to 'good-data' folder.")
+            return file_path
+        else:
+            print(f"File '{file_path}' does not exist. Skipping save_file task.")
+            return None
     else:
-        print("No files available in raw-data folder.")
+        print("No files available in raw-data folder. Skipping save_file task.")
+        return None
 
 default_args = {
-   'owner': 'airflow123',
+   'owner': 'dsp_airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 2, 24),
+    'start_date': datetime(2024, 3, 25),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 5,
     'retry_delay': timedelta(minutes=1),
 }
 
-with DAG('data_ingestion_dag3.7', default_args=default_args, schedule_interval='* * * * *') as dag:
+with DAG('data_ingestion_pipeline', default_args=default_args, schedule_interval='*/1 * * * *') as dag:
     read_data_task = PythonOperator(
         task_id='read_data',
         python_callable=read_data
     )
 
     save_file_task = PythonOperator(
-        task_id='save_file',
-        python_callable=save_file,
-        provide_context=True
+    task_id='save_file',
+    python_callable=save_file,
+    provide_context=True
     )
 
-    read_data_task >> save_file_task
+    read_data_task >> save_file_task  
