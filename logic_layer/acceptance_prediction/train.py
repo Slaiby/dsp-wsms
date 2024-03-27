d116a6270ad4eb305ccc4154f0ec8a68c8a190ab
Slaiby <mallahslaiby@gmail.com>
2024-03-27 16:21:27 +0100

    Add categorical and continuous imputers to save_model_and_preprocessors function

--- logic_layer/acceptance_prediction/train.py
+++ logic_layer/acceptance_prediction/train.py
@@ -1,5 +1,3 @@
-# train.py
-
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
@@ -13,13 +11,18 @@ def train_model(X_train_preprocessed, y_train) -> LogisticRegression:
     model.fit(X_train_preprocessed, y_train)
     return model
 
-def save_model_and_preprocessors(scaler, encoder, model) -> None:
+def save_model_and_preprocessors(scaler, encoder, model, cat_imput, contin_imput) -> None:
     dump(scaler, '../model/scaler.joblib')
     print('scaler saved')
     dump(encoder, '../model/encoder.joblib')
     print('encoder saved')
     dump(model, '../model/model.joblib')
     print('model saved')
+    dump(cat_imput, '../model/categorical_imputer.joblib')
+    print('continuous_imputer saved')
+    dump(contin_imput, '../model/continuous_imputer.joblib')
+    print('continuous_imputer saved')
+
 
 def compute_rmsle(y_test: np.ndarray, y_pred: np.ndarray, precision: int = 2) -> float:
     rmsle = np.sqrt(mean_squared_log_error(y_test, y_pred))
@@ -29,12 +32,12 @@ def compute_rmsle(y_test: np.ndarray, y_pred: np.ndarray, precision: int = 2) ->
 def build_model(data: pd.DataFrame) -> dict:
     X_train, X_test, y_train, y_test = prepare_data(data)
 
-    X_train_preprocessed, X_test_preprocessed, scaler, encoder = \
+    X_train_preprocessed, X_test_preprocessed, scaler, encoder, categorical_imputer, continuous_imputer = \
         preprocess_data(X_train, X_test)
 
     model = train_model(X_train_preprocessed, y_train)
 
-    save_model_and_preprocessors(scaler, encoder, model)
+    save_model_and_preprocessors(scaler, encoder, model, categorical_imputer, continuous_imputer)
      
     rmsle = compute_rmsle(y_test, model.predict(X_test_preprocessed))
     return {'rmsle': rmsle}
