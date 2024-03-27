3bdaf45fe5dc2168699e9e7b02293de5ee7a78ea
Slaiby <mallahslaiby@gmail.com>
2024-03-27 20:18:47 +0100

    Refactor API endpoints and update dependencies

--- api_layer/fast_api/fast_api_app.py
+++ api_layer/fast_api/fast_api_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 from api_layer.fast_api.service_slices import fetch_past_predictions, handle_csv_file, insert_inference_data
 from logic_layer.acceptance_prediction.csv_service import validate_csv
 from logic_layer.acceptance_prediction.inference import PredictionModel
-from logic_layer.pydantic_models import Prediction, PredictionRequest
+from logic_layer.pydantic_models import Prediction, PredictionQueryParams, PredictionRequest
 
 app = FastAPI()
 
@@ -19,19 +19,19 @@ async def get_predictor():
 async def predict(request_data: Union[PredictionRequest, Dict], predictor: PredictionModel = Depends(get_predictor)):
     input_dict = request_data.model_dump() if isinstance(request_data, PredictionRequest) else request_data
     prediction = predictor.make_prediction(input_dict)
-    await insert_inference_data(input_dict, prediction.tolist()[0])
+    await insert_inference_data(input_dict, prediction.tolist()[0], 'webapp')
     return {"prediction": prediction.tolist()}
 
 @app.get("/get-past-predictions", response_model=List[Prediction])
-async def get_past_predictions_endpoint():
-    predictions = await fetch_past_predictions()
+async def get_past_predictions_endpoint(params: PredictionQueryParams = Depends()):
+    predictions = await fetch_past_predictions(params)
     prediction_list = [
         Prediction(
             id=prediction['id'],
-            result=json.loads(prediction['request_data']),
+            result=json.loads(prediction.get('result', '{}')),
             prediction=str(prediction['prediction']),
             timestamp=str(prediction['timestamp'])
-        ) for prediction in predictions     
+        ) for prediction in predictions
     ]
     return prediction_list
 
@@ -62,7 +62,7 @@ async def predict_from_csv(file: UploadFile = File(...)):
                 "LoanAmount": features['LoanAmount'],
                 "Loan_Amount_Term": features['Loan_Amount_Term'],
             }
-            await insert_inference_data(response_item, prediction)
+            await insert_inference_data(response_item, prediction, 'csv-predictions')
             response_item['prediction'] = prediction == 1 and 'Eligible' or 'Not Eligible'
             response_list.append(response_item)
 
