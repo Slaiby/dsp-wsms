71621da1a87c02dda329551cc85486436d5d9518
Slaiby <mallahslaiby@gmail.com>
2024-03-27 05:13:07 +0100

    Add CSV file handling to service_slices.py

--- api_layer/fast_api/service_slices.py
+++ api_layer/fast_api/service_slices.py
@@ -1,4 +1,8 @@
 import json
+from io import BytesIO
+from fastapi import HTTPException, UploadFile
+
+from logic_layer.acceptance_prediction.csv_service import validate_csv
 from logic_layer.postgres_database.database_utils import create_db_connection
 
 async def fetch_past_predictions(page=1, page_size=10):
@@ -16,3 +20,21 @@ async def insert_inference_data(data_input, prediction):
             INSERT INTO api_inferences (request_data, prediction) VALUES ($1, $2)
         ''', json.dumps(data_input), str(prediction))
     await conn.close()
+
+async def handle_csv_file(file: UploadFile):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="File must be CSV")
+
+    file_content = await file.read()
+    MAX_FILE_SIZE_BYTES = 200 * 1024 * 1024
+
+    if len(file_content) > MAX_FILE_SIZE_BYTES:
+        raise HTTPException(status_code=413, detail="File size exceeds maximum limit")
+
+    file_in_memory = BytesIO(file_content)
+    is_valid, message = validate_csv(file_in_memory)
+    if not is_valid:
+        raise HTTPException(status_code=400, detail=message)
+    
+    file_in_memory.seek(0)
+    return file_in_memory
