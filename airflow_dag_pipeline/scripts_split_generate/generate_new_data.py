d094cfc699bd19e07c6e6cc9194c8ec4f95cb6c4
Muhammad Shaheryar <mshaheryar27@yahoo.com>
2024-05-18 00:45:30 +0200

    Updated the great expectations for the validation

--- airflow_dag_pipeline/scripts_split_generate/generate_new_data.py
+++ airflow_dag_pipeline/scripts_split_generate/generate_new_data.py
@@ -1,12 +1,34 @@
 import pandas as pd
 import numpy as np
 import os
+
 np.random.seed(42)
 num_rows = 10000
+
 def generate_missing_data(series, missing_rate=0.05):
-    mask = np.random.rand(len(series)) < missing_rate
-    series[mask] = np.nan
-    return series
+    series_copy = series.copy()
+    mask = np.random.rand(len(series_copy)) < missing_rate
+    series_copy[mask] = np.nan
+    return series_copy
+
+def introduce_data_issues(df):
+    # Introduce issues for Married column
+    df.loc[np.random.choice(df.index, size=int(num_rows * 0.01), replace=False), 'Married'] = 'Unknown'
+
+    # Introduce issues for Self_Employed column
+    df.loc[np.random.choice(df.index, size=int(num_rows * 0.01), replace=False), 'Self_Employed'] = 'Maybe'
+
+    # Introduce issues for Property_Area column
+    df.loc[np.random.choice(df.index, size=int(num_rows * 0.01), replace=False), 'Property_Area'] = 'Suburban'
+
+    # Introduce issues for Loan_Status column
+    df.loc[np.random.choice(df.index, size=int(num_rows * 0.01), replace=False), 'Loan_Status'] = 'Pending'
+
+    # Introduce issues for Dependents column
+    df.loc[np.random.choice(df.index, size=int(num_rows * 0.01), replace=False), 'Dependents'] = '4'
+
+    # Introduce issues for Credit_History column
+    df.loc[np.random.choice(df.index, size=int(num_rows * 0.01), replace=False), 'Credit_History'] = 2.0
 
 data = {
     'Loan_ID': ['LP' + str(i).zfill(6) for i in range(num_rows)],
@@ -23,13 +45,21 @@ data = {
     'Property_Area': np.random.choice(['Urban', 'Rural', 'Semiurban'], num_rows),
     'Loan_Status': np.random.choice(['Y', 'N'], num_rows)
 }
+
 df = pd.DataFrame(data)
 df['ApplicantIncome'] = generate_missing_data(df['ApplicantIncome'], missing_rate=0.02)
 df['CoapplicantIncome'] = generate_missing_data(df['CoapplicantIncome'], missing_rate=0.03)
 df['LoanAmount'] = generate_missing_data(df['LoanAmount'], missing_rate=0.03)
 df['Loan_Amount_Term'] = generate_missing_data(df['Loan_Amount_Term'], missing_rate=0.01)
+
+# Introduce data issues
+introduce_data_issues(df)
+
+# Ensure the target directory exists
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-raw_data_dir = os.path.join(parent_dir, "main-data-set")
+raw_data_dir = os.path.join(parent_dir, "main_data_set")
+os.makedirs(raw_data_dir, exist_ok=True)
+
 csv_file_path = os.path.join(raw_data_dir, 'simulated_loan_data.csv')
 df.to_csv(csv_file_path, index=False)
 
